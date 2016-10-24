0959d6b2ba3cd55109d471a532f94e66701993fc
KaiZ51 <pedrosilva510@gmail.com>
2016-10-24 23:55:49 +0100

    - Added WebViewClient.

--- app/src/main/java/pt/ismai/a26800/readr/ShowArticleActivity.java
+++ app/src/main/java/pt/ismai/a26800/readr/ShowArticleActivity.java
@@ -3,6 +3,7 @@ package pt.ismai.a26800.readr;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class ShowArticleActivity extends AppCompatActivity {
 
@@ -12,7 +13,9 @@ public class ShowArticleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_show_article);
 
         String url = getIntent().getStringExtra("url");
+
         WebView wv = (WebView) findViewById(R.id.wv_url);
+        wv.setWebViewClient(new WebViewClient());
         wv.loadUrl(url);
     }
 }
