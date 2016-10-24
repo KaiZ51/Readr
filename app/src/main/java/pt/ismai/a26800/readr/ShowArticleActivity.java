edcafe3c200b12f5e42ad9f5319661312e02dee7
KaiZ51 <pedrosilva510@gmail.com>
2016-10-24 03:03:50 +0100

    - Beginning of WebView article implementation.
    - Changed Gradle build size.

---
+++ app/src/main/java/pt/ismai/a26800/readr/ShowArticleActivity.java
@@ -0,0 +1,13 @@
+package pt.ismai.a26800.readr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_article);
+    }
+}
