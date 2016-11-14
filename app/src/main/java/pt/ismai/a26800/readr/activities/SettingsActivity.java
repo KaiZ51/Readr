8c4ea0a90d7b2b6d89d01d23c9caa898886af2f5
KaiZ51 <pedrosilva510@gmail.com>
2016-11-14 15:42:54 +0000

    - SettingsActivity

--- app/src/main/java/pt/ismai/a26800/readr/activities/SettingsActivity.java
+++ app/src/main/java/pt/ismai/a26800/readr/activities/SettingsActivity.java
@@ -1,13 +1,11 @@
 package pt.ismai.a26800.readr.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -21,31 +19,7 @@ public class SettingsActivity extends AppCompatActivity {
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
-        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);*/
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_settings, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent i = new Intent(this, SettingsActivity.class);
-            startActivity(i);
-        }
-
-        return super.onOptionsItemSelected(item);
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
     }
 
     public static class SettingsFragment extends PreferenceFragment {
